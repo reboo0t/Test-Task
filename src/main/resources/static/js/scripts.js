c6b76edc42f82797878b3e9d695f9f28274d2a4f
reboo0t <bnrebootted@gmail.com>
2024-03-05 23:18:45 +0200

    Cleared comments in scripts.js

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,21 +1,9 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-// Get all nav links
 const navLinks = document.querySelectorAll('.nav-link');
 
-// Loop through all links
 for (let i = 0; i < navLinks.length; i++) {
-    // If the href of the link matches the current URL
-    if (navLinks[i].href == window.location.href) {
-        // Add the "active" class
+    if (navLinks[i].href === window.location.href) {
         navLinks[i].classList.add('active');
     } else {
-        // Otherwise, remove it
         navLinks[i].classList.remove('active');
     }
 }
