4d60c9cdc4f6101bafd6592a9330dc14eb216be7
reboo0t <bnrebootted@gmail.com>
2024-03-14 20:53:32 +0200

    Pre-commit Spring-Security V1

---
+++ src/main/java/com/Bookery/TestTask/configs/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.Bookery.TestTask.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/", "/books", "css/**", "js/**", "covers/**").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .formLogin(withDefaults())
+                .build();
+    }
+}
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,33 +1,3 @@
-const navLinks = document.querySelectorAll('.nav-link');
-
-for (let i = 0; i < navLinks.length; i++) {
-    if (navLinks[i].href === window.location.href) {
-        navLinks[i].classList.add('active');
-    } else {
-        navLinks[i].classList.remove('active');
-    }
-}
-
-// Function to check viewport's width and add/remove class
-function checkViewport() {
-    var formElement = document.getElementById('navbar-login-button');
-    var mediaQuery = window.matchMedia('(max-width: 992px)');
-
-    if (mediaQuery.matches) {
-        formElement.classList.add('mb-2');
-    } else {
-        formElement.classList.remove('mb-2');
-    }
-}
-
-// Call the function when the page loads
-checkViewport();
-
-// Call the function when the window resizes
-window.addEventListener('resize', checkViewport);
-
-
-
 window.onload = function() {
     // Get the divs
     var cardOriginals = document.querySelectorAll('.card .cardOriginal');
